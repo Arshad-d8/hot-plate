99c0fb1ba9c9e43418c6cc0f23305dfe6cdb02d5
Arshad-d8 <71703633+Arshad-d8@users.noreply.github.com>
2025-12-10 21:54:48 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,23 +17,23 @@ export default function Navbar() {
                 <div className='flex justify-between  items-center'>
                     <p className=' text-3xl font-extrabold text-black font-sans'><span className='m-0 font-extrabold text-green-500'>H</span>OT <span className='m-0 text-green-500'>P</span>LATE</p>
 
-                    <nav className="hidden md:block ">
+                    {/* <nav className="hidden md:block ">
                         <ul className='flex font-semibold md:gap-x-5 lg:gap-x-10 text-xl justify-between'>
                             <li className='hover:text-green-500'><a href="#" className="active">Home</a></li>
                             <li className='hover:text-green-500'><a href="#">Menu</a></li>
                             <li className='hover:text-green-500'><a href="#">Services</a></li>
                             <li className='hover:text-green-500'><a href="/about">About</a></li>
                         </ul>
-                    </nav>
+                    </nav> */}
 
                     <a className='hidden md:block' href="#">
                         <button className='bg-green-500 px-10 py-2 rounded-full text-white cursor-pointer'>Contact</button>
                     </a>
-                    <div className='md:hidden text-center'>
+                    {/* <div className='md:hidden text-center'>
                         <div className=''>
                             <TiThMenu onClick={() => setShow(!show)} className='text-3xl' />
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
